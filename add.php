41b25aa720a4adc2e661d0bab8a93a85208f18a3
ryuku-saji <ryuryu30@au.com>
2023-11-16 10:48:49 +0900

    [Add] index

---
+++ add.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+    <style>
+        form{
+            background: #f8b500;
+        }
+        hr{
+            height: 5px;
+            background: black;
+            border: none;
+            align:above;
+        }
+        p1{
+            font-family: Century;
+            font-size: 40px;
+            font-weight: 600;
+            color:seagreen;
+
+        }
+        p{
+            font-weight:600;
+            color: white;
+
+        }
+    </style>
+<body>  
+    <p1> &nbsp;ADD &nbsp;</p1>
+    <hr></hr>
+    <form method="post" action="add.php">
+    <p> &nbsp;メッセージ：<input type="text" name = "Message" size="20"></p>
+    <p> &nbsp;タイトル：&nbsp;&nbsp;<input type="text" name="Title" size="20"></p>
+    <p> &nbsp;内容：</p>
+    <p><textarea name = "Text" size="200" cols="101"  rows="10"></textarea></p>
+    <p><button type='submit' name='add'>追加</button></p>
+    <p><button type='submit' name='back'>戻る</button></p>
+  </form>
+  <?php
+$dsn = "mysql:dbname=todolistsystem;port=3306;host=localhost";
+$user = "root";
+$password = "root";
+
+  if(isset($_POST['add'])){
+  //DBへの接続
+  try{
+    $dbh = new PDO( $dsn, $user, $password );
+    }catch( PDOException $error ){
+    echo "接続失敗:".$error->getMessage();
+    die();
+    }
+//変数へ格納
+    $Message = $_POST['Message'];
+    $Title = $_POST['Title'];
+    $Text = $_POST['Text'];
+    $Create = date("Y-m-d H:i:s");
+    $New = NULL;
+    
+    $sql = "INSERT INTO todolist(Title, Text, Message) VALUES (:Title, :Text, :Message)";
+    $stmt = $dbh->prepare($sql);
+    $params = array(':Title' => $Title, ':Text' => $Text, ':Message' => $Message);
+    $stmt->execute($params);
+
+    header('Location: http://localhost:8000/index.php');
+    exit;
+  }elseif(isset($_POST['back'])){
+    header('Location: http://localhost:8000/index.php');
+    exit;
+  }
+  ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,38 +1,102 @@
 <!DOCTYPE HTML>
 <html>
-</html>
     <head>
+        <meta charset="UTF-8">
+        <title>ToDoList</title>
     </head>
+        <style>
+            p{
+                font-size:40px;
+                font-weight:600;
+                font-family: Century;
+                color: seagreen;
+            }
+            table,tr,td,th{
+                border: solid 1px black; border-collapse: callapse;
+            }
+            td,th{
+                min-width: 70px;
+            }
+            td{
+                font-size: 15px;
+                background: white;
+                text-align: center;
+                color: dimgray;
+            }
+            th{
+                font-family: Georgia;
+                color: white;
+                background: #f8b500;
+            }
+            hr{
+            height: 5px;
+            background: black;
+            border: none;
+            align:center;
+            }
+        </style>
         <body>
         <?php
-$dsn = "mysql:dbname=todolistsystem;port=3307;host=localhost";
-$user = "root";
-$password = "";
+    $dsn = "mysql:dbname=todolistsystem;port=3306;host=localhost";
+    $user = "root";
+    $password = "root";
+    //DBへの接続
+    try{
+        $dbh = new PDO( $dsn, $user, $password );
+    }catch( PDOException $error ){
+        echo "接続失敗:".$error->getMessage();
+        die();
+    }
 
-try{
-    $dbh = new PDO( $dsn, $user, $password );
-}catch( PDOException $error ){
-    echo "接続失敗:".$error->getMessage();
-    die();
-}
-//対象のテーブルを変数に格納
-$sql = 'select Id, Title, Text, Created, New from TodoList';
-$stmt = $dbh->query($sql);
+    //タイトル
+    echo "<p>TODOLIST</p>\n";
+    echo "<hr></hr>";
+    
+    //追加ボタン
+    echo "<form action='index.php' method='post'>";
+    echo "<button type='submit' name='add'>追加</button>";
+    echo "</form>";
 
-echo "<table>\n";
-echo "\t<tr><th>Id</th><th>Title</th><th>Text</th><th>Created</th><th>New</th></tr>\n";
+    //対象のテーブルを変数に格納
+    $sql = "SELECT * FROM todolist";
+    $stmt = $dbh->query($sql);
+
+    //テーブルの表示
+    
+    echo "<table>\n";
+    echo "\t<tr><th>Id</th><th>Title</th><th>Text</th><th>Create</th><th>New</th><th>button</th></tr>\n";
 //FETCH＿ASSOC：カラムを１行取得する。値を取得した後はこの関数をコールするたびに次のカラムの値を返す。カラムがなくなるとNULLを返す。
 while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
     echo "\t<tr>\n";
     echo "\t\t<td>{$result['Id']}</td>\n";
     echo "\t\t<td>{$result['Title']}</td>\n";
-    echo "\t\t<td>{$result['Text']}</td>\n";
-    echo "\t\t<td>{$result['Created']}</td>\n";
+    echo "\t\t<t><td width = '100%'>{$result['Text']}</td></t>\n";
+    echo "\t\t<td>{$result['Create']}</td>\n";
     echo "\t\t<td>{$result['New']}</td>\n";
+    echo "\t\t<form action= 'index.php'  method = 'post'>\n";
+    echo "\t\t\t<td><button type='submit' name='edit'>編集</button>\n<button type='submit' name='delete'>削除</button></td>\n";
+    echo "\t\t</form>\n";
     echo "\t</tr>\n";
 }
 echo "</table>\n";
+
+        ?>
+        <?php
+        //ボタンを押した際の出力
+        $result = "";
+        if(isset($_POST['add'])){
+            header('Location: http://localhost:8000/add.php');
+            exit;
+        }elseif(isset($_POST['edit'])){
+            header('Location: http://localhost:8000/edit.php');
+            exit;
+        }elseif(isset($_POST['delete'])){
+
+            exit;
+        }
+
         ?>
         </body>
 
 
+</html>
