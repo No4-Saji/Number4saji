ab3ac76878d8d0cf207515eefb302df1623d673e
r.saji <r.saji@no4.co.jp>
2024-04-16 13:16:02 +0900

    バリデーション処理の変数名変更、リストへ遷移するリンクをhtml内へ

--- src/add_do.php
+++ src/add_do.php
@@ -6,25 +6,15 @@ require('private/ToDoList2Dao.php');
 // 変数へ格納
 $title = $_POST['Title'];
 // バリデーション処理
-$titleValidation = preg_match('/\A[[:^cntrl:]]{1,20}+\z/u', $title);
+$titleResaltOfPregMatch = preg_match('/\A[[:^cntrl:]]{1,20}+\z/u', $title);
 $document = $_POST['Doc'];
 // バリデーション処理
-$documentValidation = preg_match('/\A[[:^cntrl:]]{1,200}+\z/u', $document);
+$documentResaltOfPregMatch = preg_match('/\A[[:^cntrl:]]{1,200}+\z/u', $document);
 
-// Errorの場合0　
-if ($titleValidation == 0 || $documentValidation == 0) {
-  echo '文字数はタイトルが20字・内容が200字までです。';
-?>
-  <div class="bc"><button onclick="location.href='index.php'">リストへ戻る</button></div>
-<?php
-  exit();
-}
-?>
-<?php
 //データをカラムへ挿入
 $newDate = date("Y-m-d H:i:s");
 $editDate = null;
-if ($titleValidation == 1 && $documentValidation == 1) {
+if ($titleResaltOfPregMatch == 1 && $documentResaltOfPregMatch == 1) {
   $todoList2Dao = new ToDoList2Dao();
   $todoList2Dao->insert($title, $document, $newDate);
   echo 'データの追加が完了しました。';
@@ -39,6 +29,15 @@ if ($titleValidation == 1 && $documentValidation == 1) {
 </head>
 
 <body style="background-color:#fff3b8">
+  <?php
+  if ($titleResaltOfPregMatch == 0 || $documentResaltOfPregMatch == 0) {
+    echo '文字数はタイトルが20字・内容が200字までです。';
+  ?>
+    <div class="bc"><a class="button" href="./index.php">リストへ</a></div>
+  <?php
+    exit();
+  }
+  ?>
   <div class="bc"><a class="button" href="./index.php">リストへ</a></div>
 </body>
 
